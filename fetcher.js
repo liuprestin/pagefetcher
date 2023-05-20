bf67dc11f6a73164b2ced61b030f829ba8925253
Prestin Liu <liuprestin@proton.me>
2023-05-20 11:52:29 -0700

    creating new promise to take in a request and chain that a function that writes the website to a path specified

--- fetcher.js
+++ fetcher.js
@@ -2,27 +2,43 @@ const request = require("request");
 const fs = require("fs");
 
 //takes in two commandline arguments
-function fetcher(fetchInfo, saveFile) {
+// url eg. www.google.ca
+// path to file eg. ./index.html
+function fetcher() {
   const args = process.argv;
-  const url = args[2];
+  const url = "https://" + args[2];
   const path = args[3];
 
-  fetchInfo(url, path);
+  console.log(url);
+  console.log(path);
+
+  new Promise( (resolve, reject) => {
+
+    request(url, (error, response, body) => {
+        console.log("error:", error); // Print the error if one occurred
+        console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+        console.log("body:", body); // Print the HTML for the Google homepage.
+    
+        
+         resolve(body);
+      });
+
+
+  }
+  
+  ).then( (body) => {
+    // need to write to a file:
+  // https://nodejs.dev/en/learn/writing-files-with-nodejs/
+    fs.writeFile(`${path}`, body, (err) => {
+        if (err) {
+          console.error(err);
+        }
+        // file written successfully
+      })
+
+  }
+  
+  );
 }
 
-const fetchInfo = (url, path) => {
-  request(url, (error, response, body) => {
-    console.log("error:", error); // Print the error if one occurred
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-    console.log("body:", body); // Print the HTML for the Google homepage.
-
-    // need to write to a file:
-    // https://nodejs.dev/en/learn/writing-files-with-nodejs/
-    fs.writeFile(`${path}/index.html`, body, (err) => {
-      if (err) {
-        console.error(err);
-      }
-      // file written successfully
-    });
-  });
-};
+fetcher();
